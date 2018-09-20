ada66e6ea642e87e7578eea65d9018203a56091e
s-parkjis <s-parkjis@53S50029722.it.bsd405.org>
2018-09-20 08:26:18 -0700

    "This is my Project1."

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -15,3 +15,75 @@
  */
 
 // YOUR CODE GOES HERE :)
+
+public class ASCIIArt {
+	
+	public static void main(String[] args) {
+		star();
+		statue();
+		clothe();
+		clothe();
+		clothe();
+		clothe();
+		clothe();
+		clothe();
+		stand();
+		stand();
+		standBig();
+		stand();
+		stand();
+		stand();
+		stand();
+		stand();
+		standBig();
+		standBig();	
+		System.out.println("_____________________________");
+		water();
+		water();
+		System.out.println("   The Statue of Liberty");
+		
+	}
+	public static void statue() {
+		System.out.println("    &       *       *           *");
+		System.out.println("   [_]  *               *");
+		System.out.println("    B|       *");
+		System.out.println(" *  U|            *          *");
+		System.out.println("    ||     *");
+		System.out.println("*   || *                *");
+		System.out.println("    ||   _\\-|-|-/_           *");
+		System.out.println("    |)  * (|||||) *");
+		System.out.println("  * \\\\    | ^ ^ |   *              *");
+		System.out.println("     \\\\_   \\ u / *            *");
+		System.out.println("     L   \\__| |___      *");
+		System.out.println(" *    \\     \\_/  | )");
+		System.out.println("       \\    _/  /|/ _____");
+		System.out.println("        \\  /   | |\\/July/      *");
+		System.out.println("  *     |_/   /  |/ IV /            *");
+		System.out.println("        |    |   | ~~ /B");
+		System.out.println("      * |        |___/    *");
+		
+	} 
+	public static void star() {
+		System.out.println("  /\\       *                 *");
+		System.out.println("<    >             /\\ ");
+		System.out.println(" (/\\)      *     <    >           *");
+		System.out.println("        *         (/\\)");
+		System.out.println(" *            *        *");
+		System.out.println("    *     /\\      *          *");
+		System.out.println("        <    >   *");
+		System.out.println(" *       (/\\)       *");
+	}
+	public static void clothe() {
+		System.out.println("\t|        |");
+	}
+	public static void stand() {
+		System.out.println("      [TTTTTTTTTTTT]");
+	}
+	public static void standBig() {
+		System.out.println("    [iiiiiiiiiiiiiiii]");
+	}
+	public static void water() {
+		System.out.println("~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^");
+		System.out.println(" \" ~ \" ~ \" ~ \" ~ \" ~ \" ~ \" ~");
+	}
+}
